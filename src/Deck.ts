55ef739d8e04949c8055d4bfdc5090c16b3afee7
Andrew Sheffield <ajsheff@apple.com>
2019-03-13 21:10:07 -0700

    Added comments to shuffle funciton

--- src/Deck.ts
+++ src/Deck.ts
@@ -6,8 +6,10 @@ export class Deck {
     cards: any[] = unshuffledCards;
 
     shuffle() {
+        // Sets card as unshuffled ... not required
         this.cards = unshuffledCards;
 
+        // assigns a sort order to each card from Math.random
         const shuffledCards: Array<{ suit: string, value: string | number, sortOrder: number }> = 
             unshuffledCards.map( card => {
                 return {
@@ -16,10 +18,12 @@ export class Deck {
                 }
             });
 
+        // Sorts cards by previously assigned sort order;
         shuffledCards.sort((a, b) => {
             return a.sortOrder - b.sortOrder
         });
 
+        // Removes sortOrder and assigns new shuffled cards to this.cards
         this.cards = shuffledCards.map( card => {
             const { sortOrder, ...newCard } = card; 
             return <{suit: string, value: string | number}>newCard;
